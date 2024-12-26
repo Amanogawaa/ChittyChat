6d1b4c8c365bfd292b4613512a4b58a2f1d5c5d4
jeanne <dommolino28@gmail.com>
2024-12-26 22:49:01 +0800

    add frontend structure and files

---
+++ chittychat/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Login from "./pages/login";
+import { Route, Routes } from "react-router-dom";
+import Signup from "./pages/signup";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
